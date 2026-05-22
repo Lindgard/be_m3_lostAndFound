000bf6a8021a4cb20e4d2e552d790f205c6926eb
pisani <a.b.lindgard@gmail.com>
2026-05-22 08:23:27 +0200

    working on the service-file that will handle the logic tested in the xunit project. Fleshed out a starting DTO

--- LostAndFoundService/Models/DTOs/FoundItemDTO.cs
+++ LostAndFoundService/Models/DTOs/FoundItemDTO.cs
@@ -1,7 +1,12 @@
+using LostAndFoundService.Models.Domain;
+
 namespace LostAndFoundService.Models.DTOs;
 
 public class FoundItemDTO
 {
     public Guid Id { get; set; }
     public string Title { get; set; } = string.Empty;
+    public StatusEnum Status { get; set; }
+    public string Description { get; set; } = string.Empty;
+    public string FoundLocation { get; set; } = string.Empty;
 }
---
+++ LostAndFoundService/Services/LostAndFoundService.cs
@@ -0,0 +1,17 @@
+namespace LostAndFound.Service.Services;
+
+public class LostAndFoundService
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="LostAndFoundService"/> class.
+    /// </summary>
+    public LostAndFoundService()
+    {
+    }
+
+    public void GetAllItems()
+    {
+        var repository = new LostAndFoundRepository();
+
+    }
+}
