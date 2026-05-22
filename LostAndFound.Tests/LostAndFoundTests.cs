2b575475ca2390eb97f92bee470f7b90b35995c9
pisani <a.b.lindgard@gmail.com>
2026-05-22 08:46:41 +0200

    Red: started testing for GetAllItems method. Modified csproj to use correct service-folder

--- LostAndFound.Tests/LostAndFoundTests.cs
+++ LostAndFound.Tests/LostAndFoundTests.cs
@@ -1,4 +1,6 @@
-﻿using LostAndFound;
+﻿using LostAndFoundService.Services;
+using LostAndFoundService.Models.DTOs;
+using LostAndFoundService.Models.Domain;
 
 namespace LostAndFound.Tests;
 
@@ -8,18 +10,27 @@ public class LostAndFoundTests
     public void GetAllItems_ReturnsAllItems()
     {
         //* Arrange
-        var repository = new LostAndFoundRepository();
-        var item1 = new LostItem { Id = 1, Name = "wallet", Description = "black leather wallet", DateLost = DateTime.Now.AddDays(-1) };
-        var item2 = new LostItem { Id = 2, Name = "keys", Description = "set of car keys", DateLost = DateTime.Now.AddDays(-2) };
-        repository.AddItem(item1);
-        repository.AddItem(item2);
+        var service = new LostAndFoundService();
+        var item1 = new FoundItemDTO
+        {
+            Id = Guid.NewGuid(),
+            Title = "Wallet",
+            Status = StatusEnum.Available,
+            Description = "Black leather wallet found in classroom 101.",
+            FoundLocation = "Classroom 101"
+        };
+        var item2 = new FoundItemDTO
+        {
+            Id = Guid.NewGuid(),
+            Title = "Phone",
+            Status = StatusEnum.Available,
+            Description = "iPhone found in the school library.",
+            FoundLocation = "School Library"
+        };
 
         //* Act
-        var items = repository.GetAllItems();
+        service.GetAllItems();
 
         //* Assert
-        Assert.Equal(2, items.Count);
-        Assert.Contains(items, i => i.Id == item1.Id && i.Name == item1.Name && i.Description == item1.Description && i.DateLost == item1.DateLost);
-        Assert.Contains(items, i => i.Id == item2.Id && i.Name == item2.Name && i.Description == item2.Description && i.DateLost == item2.DateLost);
     }
 }
