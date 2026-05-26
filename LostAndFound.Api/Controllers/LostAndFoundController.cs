d5594223e6e921208ac8d655c258ec0f82dbb24a
ALP <a.b.lindgard@gmail.com>
2026-05-26 12:09:43 +0200

    removed boilerplate from controller in api

--- LostAndFound.Api/Controllers/LostAndFoundController.cs
+++ LostAndFound.Api/Controllers/LostAndFoundController.cs
@@ -1,36 +1,10 @@
 using Microsoft.AspNetCore.Mvc;
 
-<<<<<<< HEAD
-namespace LostAndFound.Api.Controllers;
-
-[ApiController]
-[Route("api/[controller]")]
-public class LostAndFoundController : ControllerBase
-{
-
-}
-=======
 namespace LostAndfound.Api.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class WeatherForecastController : ControllerBase
+public class LostAndFoundController : ControllerBase
 {
-    private static readonly string[] Summaries =
-    [
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    ];
 
-    [HttpGet(Name = "GetWeatherForecast")]
-    public IEnumerable<WeatherForecast> Get()
-    {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        {
-            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
-    }
 }
->>>>>>> 4861a71 (added webapi to solution for future expansion of project.)
