a7676b1538f9d62459ddf5c32e2484cf3d07ffdf
ALP <a.b.lindgard@gmail.com>
2026-05-26 11:29:31 +0200

    working on merge conflict

--- LostAndFound.Api/Program.cs
+++ LostAndFound.Api/Program.cs
@@ -1,6 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+<<<<<<< HEAD
+=======
+
+builder.Services.AddControllers();
+>>>>>>> 4861a71 (added webapi to solution for future expansion of project.)
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
@@ -14,4 +19,11 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+<<<<<<< HEAD
+=======
+app.UseAuthorization();
+
+app.MapControllers();
+
+>>>>>>> 4861a71 (added webapi to solution for future expansion of project.)
 app.Run();
