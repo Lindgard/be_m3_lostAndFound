5a511d5ad15e31b47f69faa8c8c32cff163537cc
ALP <a.b.lindgard@gmail.com>
2026-05-27 12:36:13 +0200

    working on GetAllItems method, it returns the exception for an empty repository

--- LostAndFoundService/Services/LostAndFoundService.cs
+++ LostAndFoundService/Services/LostAndFoundService.cs
@@ -22,6 +22,10 @@ public class LostAndFoundService
     /// <returns>A list of all found items.</returns>
     public List<FoundItemDTO> GetAllItems()
     {
+        if (_items.Count == 0)
+        {
+            throw new InvalidOperationException("No items found in the repository.");
+        }
         return _items;
     }
 
