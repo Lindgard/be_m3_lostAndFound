53a7f44dca8bd8adf2405391ba2d291df369aa05
ALP <a.b.lindgard@gmail.com>
2026-06-29 11:54:44 +0200

    modified launchsettings and Program.cs to make swagger work and make the program not run into errors on launch

--- LostAndFound.Api/Program.cs
+++ LostAndFound.Api/Program.cs
@@ -8,16 +8,11 @@ builder.Services.AddScoped<LostAndFoundService>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
     app.UseSwagger();
     app.UseSwaggerUI();
 }
